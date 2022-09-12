3a79da892510a688acc1fed17044f4aa34690824
Cwolf <l20140919@queretaro.tecnm.mx>
2022-09-12 16:34:59 -0500

    Termindado cuarto requerimiento

--- prueba.cpp
+++ prueba.cpp
@@ -8,7 +8,10 @@ float radio, altura, x;
 // Este programa calcula el volumen de un cilindro.
 void main()
 {
-    a=(3+5)*8-(10-4)/2; 
+    x = 10;
+    a=(3+5)*8-(x-4)/2;
+    printf("resultado = "); 
+    printf((3+5)*8-(x-4)/2);
     b=(10+10);
     printf("hola");
     printf("hola");
